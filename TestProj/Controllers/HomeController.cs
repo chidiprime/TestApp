ffcbb60a44264129ec2ee4cc41d1cefc87343e78
Daniel Onyeachonam <daniel.onyeachonam@VentureGardenGroup.Local>
2019-10-25 14:30:58 +0100

    Initial Commit

---
+++ TestProj/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TestProj.Models;
+
+namespace TestProj.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        
+        public IActionResult Index()
+
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<IActionResult> PostCredential([FromBody]ViewModel viewModel)
+        {
+            HttpResponseMessage response;
+            try
+            {
+                string url = "identity/connect/token";
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(_configuration.GetValue<string>("Baseurl"));
+                    client.DefaultRequestHeaders.Clear();
+                    var requestBody = new List<KeyValuePair<string, string>>();
+                    requestBody.Add(new KeyValuePair<string, string>("grant_type", viewModel.grant_type));
+                    requestBody.Add(new KeyValuePair<string, string>("username", viewModel.username));
+                    requestBody.Add(new KeyValuePair<string, string>("password", viewModel.password));
+                    requestBody.Add(new KeyValuePair<string, string>("scope", viewModel.scope));
+                    requestBody.Add(new KeyValuePair<string, string>("client_id", viewModel.client_id));
+                    requestBody.Add(new KeyValuePair<string, string>("client_secret", viewModel.client_secret));
+                    //  client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");                  
+                    var request = new HttpRequestMessage(HttpMethod.Post, _configuration.GetValue<string>("Baseurl") + url)
+                    {
+                        Content = new System.Net.Http.FormUrlEncodedContent(requestBody)
+                    };
+                    response = await client.SendAsync(request);
+                }
+                return StatusCode((int)response.StatusCode, response.Content.ReadAsStringAsync().Result);
+            }
+            catch (Exception ex)
+            {
+
+                return new StatusCodeResult(500);
+            }
+
+        }
+
+    }
+}
---
+++ TestProj/wwwroot/js/Test.js
@@ -0,0 +1,68 @@
+﻿
+$(document).ready(function () {
+    $('#grant_type').val('password');
+    $('#username').val('lolade.amunigun@fidesicng.com');
+    $('#password').val('P@ssword01');
+    $('#scope').val('openid profile identity-server-api');
+    $('#clientid').val('pmsapi');
+    $('#clientsecret').val('SolutionPms');
+});
+$('#save').click(function () {
+    $('#result').text('');
+    var grant_type = $('#grant_type').val();
+    var username = $('#username').val();
+    var password = $('#password').val();
+    var scope = $('#scope').val();
+    var clientid = $('#clientid').val();
+    var clientsecret = $('#clientsecret').val();
+    if (grant_type == "") {
+        alert("Grant type is required");
+        return;
+    }
+    if (username == "") {
+        alert("Username is required");
+        return;
+    }
+    if (password == "") {
+        alert("Password is required");
+        return;
+    }
+    if (scope == "") {
+        alert("Scope is required");
+        return;
+    }
+    if (clientid == "") {
+        alert("Client Id is required");
+        return;
+    }
+    if (clientsecret == "") {
+        alert("Client Secret is required");
+        return;
+    }
+    var sosObject =
+    {
+        grant_type: grant_type,
+        username: username,
+        password: password,
+        scope: scope,
+        client_id: clientid,
+        client_secret: clientsecret
+    }
+    $("#LoadingImage").show();
+    $.ajax({
+        url: "/PostCredential",
+        type: 'POST',
+        contentType: "application/json",
+        data: JSON.stringify(sosObject),
+        success: function (data, textStatus, xhr) {
+            $('#result').text(data);
+            //$("#LoadingImage").hide();
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            $('#result').text(xhr.responseText);
+        },
+        complete: function () {
+            //$("#LoadingImage").hide();
+        }
+    });
+});
